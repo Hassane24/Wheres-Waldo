dd0862df0918f37ed98ee5b2cba47f586499cb03
Hassane <hsisane5@gmail.com>
2023-01-05 15:25:52 +0100

    added charname div to charholder component

--- src/components/CharacterHolder.js
+++ src/components/CharacterHolder.js
@@ -1,6 +1,7 @@
-const CharacterHolder = ({ image }) => (
+const CharacterHolder = ({ image, charName }) => (
   <div className="char-holder">
     <img src={image} alt="" className="char" />
+    <div className="char-name">{charName}</div>
   </div>
 );
 export default CharacterHolder;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import CharacterHolder from "./CharacterHolder";
+import "../styles/Navbar.css";
 import noFace from "../assets/no-face.jpg";
 import saitama from "../assets/saitama.jpg";
 import vash from "../assets/vash-the-stampede.jpg";
 const NavBar = ({ charsLeft }) => (
   <div className="nav-bar">
-    <CharacterHolder image={noFace} />
-    <CharacterHolder image={saitama} />
-    <CharacterHolder image={vash} />
+    <CharacterHolder image={noFace} charName="No face" />
+    <CharacterHolder image={saitama} charName="Saitama" />
+    <CharacterHolder image={vash} charName="Vash the stampede" />
     <div className="chars-left">{charsLeft}</div>
   </div>
 );
