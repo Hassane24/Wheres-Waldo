87bf6c28e96a6bdcef2bc67071b0d31f157c3858
Hassane <hsisane5@gmail.com>
2023-01-05 15:13:42 +0100

    added charholder component and refactored navbar

---
+++ src/components/CharacterHolder.js
@@ -0,0 +1,6 @@
+const CharacterHolder = ({ image }) => (
+  <div className="char-holder">
+    <img src={image} alt="" className="char" />
+  </div>
+);
+export default CharacterHolder;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,12 @@
+import CharacterHolder from "./CharacterHolder";
+import noFace from "../assets/no-face.jpg";
+import saitama from "../assets/saitama.jpg";
+import vash from "../assets/vash-the-stampede.jpg";
 const NavBar = ({ charsLeft }) => (
   <div className="nav-bar">
-    <div className="char-holder">
-      <img src="" alt="" className="char" />
-    </div>
-    <div className="char-holder">
-      <img src="" alt="" className="char" />
-    </div>
-    <div className="char-holder">
-      <img src="" alt="" className="char" />
-    </div>
+    <CharacterHolder image={noFace} />
+    <CharacterHolder image={saitama} />
+    <CharacterHolder image={vash} />
     <div className="chars-left">{charsLeft}</div>
   </div>
 );
