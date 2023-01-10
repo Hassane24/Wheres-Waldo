de19abe3af553ac77958f7c9edcd18b04cef8664
Hassane <hsisane5@gmail.com>
2023-01-10 16:23:08 +0100

    credit component

---
+++ src/components/MadeBy.js
@@ -0,0 +1,13 @@
+const MadeBy = () => (
+  <>
+    <div className="made-by">
+      <span>
+        Image by
+        <a href="https://www.instagram.com/ad.2.222/" target="_blank">
+          Egor Klyuchnyk
+        </a>
+      </span>
+    </div>
+  </>
+);
+export default MadeBy;
